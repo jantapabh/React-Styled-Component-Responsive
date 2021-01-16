c8ece3477de8381315d4124c034f67194942c51d
jantapabh <s6035512034@phuket.psu.ac.th>
2021-01-17 01:45:03 +0700

    add item

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {
     FooterContainer,
     FooterWrapper,
@@ -6,8 +6,15 @@ import {
     FooterLinkItems,
     FooterLinkTitle,
     FooterLinksContainer,
-    FooterLinksWrapper
+    FooterLinksWrapper,
+    SocialMedia,
+    SocialIconLink,
+    SocialLogo,
+    SocialIcons,
+    SocialMediaWrap,
+    WebsiteRight
 } from './FooterElements'
+import { FaFacebook, FaYoutube, FaTwitter, FaInstagram, FaLinkedin} from 'react-icons/fa'
 
 
 const FooterSection = () => {
@@ -27,36 +34,61 @@ const FooterSection = () => {
                             <FooterLink to="/sigin">Terms of Service</FooterLink>
                         </FooterLinkItems>
                         <FooterLinkItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to="/sigin">About us</FooterLink>
-                            <FooterLink to="/sigin">How it work</FooterLink>
-                            <FooterLink to="/sigin">Testimonials</FooterLink>
-                            <FooterLink to="/sigin">Careers</FooterLink>
-                            <FooterLink to="/sigin">Investors</FooterLink>
-                            <FooterLink to="/sigin">Terms of Service</FooterLink>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                            <FooterLink to="/sigin">Contact</FooterLink>
+                            <FooterLink to="/sigin">Support</FooterLink>
+                            <FooterLink to="/sigin">Destinations</FooterLink>
+                            <FooterLink to="/sigin">Sponsorships</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                     <FooterLinksWrapper>
                         <FooterLinkItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to="/sigin">About us</FooterLink>
-                            <FooterLink to="/sigin">How it work</FooterLink>
-                            <FooterLink to="/sigin">Testimonials</FooterLink>
-                            <FooterLink to="/sigin">Careers</FooterLink>
-                            <FooterLink to="/sigin">Investors</FooterLink>
-                            <FooterLink to="/sigin">Terms of Service</FooterLink>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                            <FooterLink to="/sigin">Submit Video</FooterLink>
+                            <FooterLink to="/sigin">Ambassadors</FooterLink>
+                            <FooterLink to="/sigin">Agency</FooterLink>
+                            <FooterLink to="/sigin">Influencer</FooterLink>
                         </FooterLinkItems>
                         <FooterLinkItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to="/sigin">About us</FooterLink>
-                            <FooterLink to="/sigin">How it work</FooterLink>
-                            <FooterLink to="/sigin">Testimonials</FooterLink>
-                            <FooterLink to="/sigin">Careers</FooterLink>
-                            <FooterLink to="/sigin">Investors</FooterLink>
-                            <FooterLink to="/sigin">Terms of Service</FooterLink>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                            <FooterLink to="/sigin">Instagram</FooterLink>
+                            <FooterLink to="/sigin">Facebook</FooterLink>
+                            <FooterLink to="/sigin">Youtube</FooterLink>
+                            <FooterLink to="/sigin">Twitter</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to="/">
+                            Dolla
+                        </SocialLogo>
+                        <WebsiteRight>Dolla : 2020 
+                        All rights reserved.</WebsiteRight>
+                        <SocialIcons>
+                            <SocialIconLink href="/" target="_blank"
+                            aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank"
+                            aria-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank"
+                            aria-label="Youtube">
+                                <FaYoutube />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank"
+                            aria-label="Twitter">
+                                <FaTwitter />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank"
+                            aria-label="Linkedin">
+                                <FaLinkedin />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
             </FooterWrapper>
         </FooterContainer>
     )
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -11,23 +11,24 @@ import {
 } from './SidebarElements'
 
 const Sidebar = ({ isOpen, toggle}) => {
+
     return (
         <SidebarContainer isOpen={isOpen} onClick={toggle}>
-            <Icon onClick={toggle}>
+            <Icon toggle={toggle}>
                 <CloseIcon />
             </Icon>
             <SidebarWrapper>
                 <SidebarMenu>
-                    <SidebarLink to="about" onClick={toggle}>
+                    <SidebarLink to="/about" onClick={toggle}>
                         About
                     </SidebarLink>
-                    <SidebarLink to="discover" onClick={toggle}>
+                    <SidebarLink to="/discover" onClick={toggle}>
                         Discover
                     </SidebarLink>
-                    <SidebarLink to="service" onClick={toggle}>
+                    <SidebarLink to="/service" onClick={toggle}>
                         Service
                     </SidebarLink>
-                    <SidebarLink to="signup" onClick={toggle}>
+                    <SidebarLink to="/signup" onClick={toggle}>
                         Sign Up
                     </SidebarLink>
                 </SidebarMenu>
